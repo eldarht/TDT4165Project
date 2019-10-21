fd141d3d9fee0f9e228ca7c9eb8e7ab363ab9b62
Eldar Hauge Torkelsen <eldarht@stud.ntnu.no>
2019-10-21 13:44:20 +0200

    Added answers for task1

---
+++ tasks/task1.scala
@@ -0,0 +1,65 @@
+/** Provides solutions for task 1 a-d. */
+object task1 extends App {
+
+	/** Uses a for loop to get the sum of an Int array.
+	 *
+	 * @param a Array to sum
+	 *
+	 * @return Sum of array
+	 */
+	def sumFor(a: Array[Int]): Int = {
+		var result : Int = 0;
+
+		for(i <- 0 to a.length-1){
+			result += a(i);
+		}
+		return result;
+	}
+
+	/** Uses a for recurcion to get the sum of an Int array.
+	 *
+	 * @param a Array to sum
+	 *
+	 * @return Sum of array
+	 */
+	def sumRecurcive(a: Array[Int]): Int = {
+		if(a.length > 1){
+			a(0) + sumRecurcive(a.drop(1));
+		}else{
+			a(0);
+		}
+	}
+
+	/** Naive calculation of the nth fibonacci number without optimization.
+	 *
+	 * BigInt is different from Int in that it can take significantly larger numbers.
+	 * 
+	 * @param n The position of the number in the set of fibonacci numbers to be computed.
+	 *
+	 * @return The nth fibonacci number.
+	 */
+	def fibonacci(n: BigInt): BigInt = {
+		if(n == 0){
+			return 0;
+		}else if(n == 1){
+			return 1;
+		}else{
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
+	}
+
+	val a = new Array[Int](50);
+
+	for(i <- 1 to 50){
+		a(i-1)= i;
+	}
+
+	val result: String = a.mkString(", ");
+
+	println(s"The array is: [$result]");
+	printf("The sum of the array is: %d\n", sumFor(a));
+	printf("The sum of the array is: %d\n", sumRecurcive(a));
+	printf("The 10th fibonacci number is: %d\n", fibonacci(10));
+  
+}
+
