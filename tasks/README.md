af4f5d905a8688384f8576018de6ea174ebee7b9
Eldar Hauge Torkelsen <eldarht@stud.ntnu.no>
2019-10-21 16:27:48 +0200

    Answered task2 a and b

---
+++ tasks/README.md
@@ -0,0 +1,7 @@
+# Tasks
+
+This folder contains sourcefiles and documentation for Task 1 and 2.
+
+- [task1.scala](task1.scala) Shows basic language features in scala, like function definition and recursion.
+- [task2.scala](task2.scala) Shows the use of basic multithreading in scala.
+- [raceConditionTest.sh](raceConditionTest.sh) Script for running the code a 100 times. Meant to be used to check for race conditions in task2.
---
+++ tasks/task2.scala
@@ -0,0 +1,54 @@
+/** Provides solutions for task 2 a-d. */
+object task2 extends App {
+	/** Creates a thread that can run a given function.
+	 *
+	 * This function is a rewrite of the one given in the project_task
+	 * Main.scala file.
+	 *
+	 * @param      function  The function the thread can execute.
+	 *
+	 * @return     The runnable thread (not yet started).
+	 */
+	def createThread(function: => Unit): Thread = {      
+		val t = new Thread {
+            override def run() = function;
+        }
+        return t;
+    }
+
+    /** Counter to be manipulated by two threads
+     * 
+     * Without synchronization the result might be dependent on a race condition.
+     * Meaning that the result varies depending on where the switching of the threads happens
+     * or if they execute read/writes at the same time.
+     * 
+     * This can, as an example, be problematic your exit condition from a loop is i == 0.
+     * The counter can be changed twice before the next itteratin, going from i==1 to i==-1,
+     * creating an infinite loop.
+     */
+    private var counter: Int = 0;
+
+    /** Increases the encapsulating objects counter variable.
+     * 
+     */
+	def increaseCounter(): Unit = {
+		counter += 1;
+	}
+
+	def readCounter(): Unit = {
+		printf("The current value of counter is: %d\n", this.counter);
+	}
+
+
+    val thread = createThread(printf("Thread has been run\n"));
+    
+    println(s"The created thread is: $thread");
+
+    val increaseThread1 = createThread(increaseCounter());
+    val increaseThread2 = createThread(increaseCounter());
+    val readThread = createThread(readCounter());
+
+   	increaseThread1.start();
+   	increaseThread2.start();
+   	readThread.start();
+}
